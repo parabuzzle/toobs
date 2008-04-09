ab7cc6d7a447498f9b91b5a480778cd11d9fa6af
jnorthcote <jnorthcote@aeb1badb-362c-0410-94d3-b96eb40a0612>
2008-04-09 00:29:07 +0000

    Removing implementation specific methods from IRequest and BaseRequest Removed or moved most of PlatformConstants that are implementation specific or should be configurable Left PERSON_ID_NO_PERSON simply because it would be a pain to remove right now Cleaned up some stuff in DateHelper along with changing to Configuration for Date formats
    TODO: DatasourceImpl setting parameters for created date, modified date, creator, modifier and personId are all implementation specific
    
    
    
    
    git-svn-id: https://toobs.svn.sourceforge.net/svnroot/toobs@6 aeb1badb-362c-0410-94d3-b96eb40a0612

--- Toobs/Platform/Base/src/org/toobs/framework/util/IRequest.java
+++ Toobs/Platform/Base/src/org/toobs/framework/util/IRequest.java
@@ -19,24 +19,6 @@ public interface IRequest {
   
   public abstract void setHttpResponse(HttpServletResponse httpResponse);
 
-  public abstract String getClientChangeParam();
-
-  public abstract String[] getClientListParam();
-
-  public abstract String getClientParam();
-
-  public abstract Object getClient();
-
-  public abstract String getPersonId();
-  
-  public abstract Object getPerson();
-  
-  public abstract String getPersonBusinessId();
-
-  public abstract String getBusinessTemplateId();
-  
-  public abstract String getSecurityMode();
-
   public abstract Boolean getSingleBooleanParam(String paramName);
 
 }
